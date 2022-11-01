c5a5fe894328591d3ac75ab687558924d5f853cc
DVDRpal12321 <danilovukovic.95@gmail.com>
2022-11-01 23:44:46 +0100

    -message placeholder

--- react-project-filesystem/src/App.js
+++ react-project-filesystem/src/App.js
@@ -11,11 +11,10 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://trucksllc.com/"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        >Trucks current
         </a>
       </header>
     </div>
