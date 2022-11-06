12387173a0e243ce0ef90863dd8ffcf43e54d409
DVDRpal12321 <danilovukovic.95@gmail.com>
2022-11-06 22:48:23 +0100

    ne znam vise sta sam menjao

--- react-project-filesystem/src/components/MainScreen/main-screen.jsx
+++ react-project-filesystem/src/components/MainScreen/main-screen.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import './main-screen.css'
 import img1 from '../../assets/viber_image_2022-11-03_00-14-39-232.jpg'
+import Button from '@mui/material/Button';
+//import styled from '@emotion/styled'
 
+// const Mainbutton = styled.button`
+//   padding: 15px;
+//   background-color: rgba(207, 1, 1, 0.75) ;
+//   font-size: 24px;
+//   border-rad
+//   color: black;ius: 4px;
+//   font-weight: bold;
+//   &:hover {
+//     background-color: rgb(207, 1, 1);
+//     ;
+//   }
 
 const Mainscreen = () => {
 return(
@@ -14,7 +27,26 @@ return(
         <div className="content">
             <h1>Transportation done</h1> 
             <h1 className="h1s">Right</h1> 
+            <Button 
+            sx={{
+                position:'absolute',
+                top:"92px",
+                left:"380px"
+            }} 
+            variant="contained"
+            color = "error"
+            >
+            Join us
+            </Button>
+            
+            <div className="description">
+            <p>
+            We provide safe and reliable transportation services to a diverse group of
+            customers throughout the continental United States, Canada and Mexico.
+            </p>
         </div>
+        </div>
+        
 
     </div>
 );
