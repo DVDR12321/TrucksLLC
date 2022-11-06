1e95c092de10b3d8faa13190a193cadf128c10c4
DVDRpal12321 <danilovukovic.95@gmail.com>
2022-11-06 20:35:06 +0100

    image & title

--- react-project-filesystem/src/components/MainScreen/main-screen.jsx
+++ react-project-filesystem/src/components/MainScreen/main-screen.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import './main-screen.css'
 import img1 from '../../assets/viber_image_2022-11-03_00-14-39-232.jpg'
 
+
 const Mainscreen = () => {
 return(
-    <div>
-        <image
+    <div className="main">
+        <img
+            className="img1"
              src={img1}
+             alt = ''
             />
+        <div className="content">
+            <h1>Transportation done</h1> 
+            <h1 className="h1s">Right</h1> 
+        </div>
 
     </div>
 );
