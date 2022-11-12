e8c1f0689aea902987729f50b4541beee9534ee8
DVDRpal12321 <danilovukovic.95@gmail.com>
2022-11-12 03:52:17 +0100

    3rd page done

--- react-project-filesystem/src/components/offers/offers.jsx
+++ react-project-filesystem/src/components/offers/offers.jsx
@@ -4,14 +4,14 @@ import './offers.css'
 
 const Offers = () => {
     return (
-        <Mycontainer Header = "What we offer">
-            <p className = "list">
+        <Mycontainer Header = "What we offer" Body = {<p className = "list">
             <li>24 hours Dispatch</li> 
             <li>Fleet Monitoring Systems</li> 
             <li>Know where your load is any given time</li>  
             <li>Well maintained fleet</li> 
             <li>Responsible drivers</li> 
-            </p>
+            </p>}>
+            
         </Mycontainer>
     )
 };
