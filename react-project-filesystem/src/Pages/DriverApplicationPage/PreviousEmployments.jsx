5b799556f27bc471a53f31ccb1a3e7cd0ec4a5df
DVDRpal12321 <danilovukovic.95@gmail.com>
2022-12-16 18:29:31 +0100

    added link parsing

--- react-project-filesystem/src/Pages/DriverApplicationPage/PreviousEmployments.jsx
+++ react-project-filesystem/src/Pages/DriverApplicationPage/PreviousEmployments.jsx
@@ -1,24 +1,34 @@
-import { Autocomplete, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 // 85a1a1c4c94d781917fdf0ddb5bc09d49092c384
 
 const PreviousEmployments = () => {
-  const [name, setName] = useState();
+  const [name, setName] = useState("");
+  const [CarrierData, setCarrierData] = useState({});
+
+  const HandleChange = (e) => {
+    return setName(e.target.value);
+  };
   return (
     <div>
+      <TextField
+        onChange={(e) => {
+          HandleChange(e);
+        }}
+      ></TextField>
       {useEffect(() => {
         fetch(
-          "https://mobile.fmcsa.dot.gov/qc/services/carriers/2530313?webKey=85a1a1c4c94d781917fdf0ddb5bc09d49092c384"
+          `https://mobile.fmcsa.dot.gov/qc/services/carriers/name/${name.toString()}?webKey=85a1a1c4c94d781917fdf0ddb5bc09d49092c384`
         )
           .then((response) => {
             return response.json();
           })
-          .then((data) => setName(data));
-      }, [])}
-      {console.log(name)}
-      <TextField value={name.content.carrier.dotNumber}></TextField>
+          .then((data) => console.log(data));
+        // .then((data) => setCarrierData(data))
+        // .then(console.log(CarrierData));
+      }, [name])}
     </div>
   );
 };
