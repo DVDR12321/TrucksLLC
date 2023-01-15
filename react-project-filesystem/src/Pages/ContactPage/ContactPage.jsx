8cf00ee98845cbffd518cfba27a850db2d589447
DVDRpal12321 <danilovukovic.95@gmail.com>
2023-01-15 01:19:48 +0100

    bye bye

--- react-project-filesystem/src/Pages/ContactPage/ContactPage.jsx
+++ react-project-filesystem/src/Pages/ContactPage/ContactPage.jsx
@@ -1,14 +1,4 @@
 import React from "react";
-import Grid from '@mui/material/Grid';
-import { StyledDiv } from "./StyledComponents";
-import GoogleMaps from "./GoogleMaps";
-import SimpleMap from "./GoogleMaps";
 
-const ContactPage = () => {
-  return (
-    <SimpleMap></SimpleMap>
-    
-  );
-    
-};
+const ContactPage = () => {};
 export default ContactPage;
